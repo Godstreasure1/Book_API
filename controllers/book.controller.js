f66c396aff634f4388cc74d16bfe170e063d7901
Godstreasure1 <atreasure955@gmail.com>
2023-06-26 08:29:28 +0100

    implementation of BookUpdateByid

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -32,12 +32,34 @@ const getBooks = async (_, res) => {
 };
 
 const updateBookById = async (req, res) => {
-  // complete this function
-};
+  
+  try{
+    const book = await Book_API.findById(id);
+    
+    if (!note) {
+      return res.status(400).json({ message:`${id} information does not exist`});
+  }
 
-const deleteBookById = async (req, res) => {
-  // complete this function
+   book.author = author;
+   await book.save();
+
+   return res.status(201).json ({message: `${id} book updated successfully`, book: book});
+} catch (error) {
+  if (error.kind ==="objectid") {
+    return res.status (400).json({message:`${id} information does not exist`});
+  }
+  return res.status(500).json({ message:error});
+
+  }
+
 };
+  
+ 
+const deleteBookById = async (req, res) => {
+//complete this function
+
+}
+  
 
 module.exports = {
   addBook,
